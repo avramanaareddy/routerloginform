38012cff0fc22d8052ae61814a110229570de2d4
Vennkata <avramanaareddy@yahoo.com>
2020-10-22 17:04:04 -0400

    Prompt usage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,103 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
-
-export default function App() {
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+  Redirect,
+  Prompt
+} from "react-router-dom";
+const User = (params) => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>The User name is {params.username}</h1>
     </div>
   );
+};
+export default class App extends Component {
+  state = {
+    loggedIn: false
+  };
+  loginHandle = () => {
+     this.setState(prevState=>({
+loggedIn:!prevState.loggedIn
+     }))
+  };
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/" exact activeStyle={{ color: "green" }}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" exact activeStyle={{ color: "green" }}>
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/user/username"
+                  exact
+                  activeStyle={{ color: "green" }}
+                >
+                  User
+                </NavLink>
+              </li>
+            </ul>
+            <Prompt 
+            when={!this.state.loggedIn} 
+            message={(location)=>{
+              return location.pathname.startsWith("/user")? 'Are you sure?':true
+            }}/>
+          </nav>
+          <input
+            type="button"
+            value={this.state.loggedIn ? "Log out" : "Log in"}
+            onClick={this.loginHandle.bind(this)}
+          />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => {
+                return <div> This is Home page</div>;
+              }}
+            />
+            {/*
+  When you use exact strict, in the browser 
+  if you give the url path as /about, then it 
+  is not going to work, you must provide the 
+  /about/ to execute the path
+ */}
+            <Route
+              path="/about"
+              exact
+              strict
+              render={() => {
+                return <div> This is About </div>;
+              }}
+            />
+            <Route
+              path="/user/:username"
+              exact
+              strict
+              render={({ match }) => {
+                return this.state.loggedIn ? (
+                  <User username={match.params.username} />
+                ) : (
+                  <Redirect to="/" />
+                );
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
